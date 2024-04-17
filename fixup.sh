0f2eb1782261193b9e821e6c6ad5af7062026347
emmanuelryan-gamla <emmanuelryan.gamla@hcl.com>
2024-04-18 02:20:39 +0800

    Update fixup script to handle missing directories on Github workflow

--- fixup.sh
+++ fixup.sh
@@ -2,14 +2,18 @@
 # Copyright (C) 2023 HCL America Inc.                                        #
 # Apache-2.0 license   https://www.apache.org/licenses/LICENSE-2.0           #
 # ========================================================================== #
+if [ -d dist/cjs/ ]; then
 cat >dist/cjs/package.json <<!EOF
 {
-    "type": "commonjs"
+  "type": "commonjs"
 }
 !EOF
+fi
 
+if [ -d dist/esm/ ]; then
 cat >dist/esm/package.json <<!EOF
 {
-    "type": "module"
+  "type": "module"
 }
 !EOF
+fi
