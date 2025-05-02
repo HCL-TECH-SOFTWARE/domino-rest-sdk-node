c9bdc9246fc246481a6fab1a8a87209d47d90594
emmanuelryan-gamla <emmanuelryan.gamla@hcl.com>
2025-05-02 21:33:30 +0800

    Add fetch mode and setting of Origin header in DominoRequestOptions

--- samples/resources/credentials.js
+++ samples/resources/credentials.js
@@ -3,7 +3,7 @@
  * Apache-2.0 license   https://www.apache.org/licenses/LICENSE-2.0           *
  * ========================================================================== */
 
-import credentialsJson from '../../credentials.json' assert { type: 'json' };
+import credentialsJson from '../../credentials.json' with { type: 'json' };
 
 export const getCredentials = () => {
   let credentialsToUse;
